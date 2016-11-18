881b892a0895895704a24662c42278c32a0965e3
Jonathan Aguirre <jonathanaguirreglez@gmail.com>
2016-11-18 02:11:58 -0800

    Ejemplo Funcionando
    Funcionando la interfaz grafica.

--- www/js/AppController.js
+++ www/js/AppController.js
@@ -2,43 +2,68 @@ angular.module('app.controllers', [])
   .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
   })
-  .controller('RutasCtrl', function ($scope, $stateParams) {
+  .controller('RutasCtrl', function ($scope, $stateParams, $rootScope, userInfo) {
     $scope.myTitle = "Rutas";
+    $scope.rutasConfigs = userInfo.getRutas();
+    $rootScope.$on('$stateChangeStart',
+      function (event, toState, toParams, fromState, fromParams) {
+        console.log(toState);
+        if (toState.name === 'app.rutas') {
+          $scope.rutasConfigs = userInfo.getRutas();
+          console.log($scope.rutasConfigs[0]);
+        }
+      });
+
+    console.log($scope.rutasConfigs[0]);
   })
 
 
-.controller('ConfigRutaCtrl', function ($scope, rutasWaypoints) {
+.controller('ConfigRutaCtrl', function ($scope, rutasWaypoints, userInfo) {
     $scope.myTitle = "Seleccionar Ruta";
 
     $scope.rutas = rutasWaypoints.getRutas();
+
+
+
   })
-  .controller('ConfigCtrl', function ($scope, $state, $stateParams, $cordovaGeolocation, rutasWaypoints) {
+  .controller('ConfigCtrl', function ($scope, $state, $stateParams, $cordovaGeolocation, $ionicPopup,$timeout, rutasWaypoints, userInfo) {
     var ruta = rutasWaypoints.getRuta($stateParams.ruta);
+
     $scope.myTitle = ruta.title;
     $scope.seleccionarPunto = "";
     $scope.rutas = rutasWaypoints.getRutas();
+    $scope.ruta = {
+      puntos: {},
+      rutaId: ruta.title
+    };
     $scope.gMarkers = {};
+    $scope.pos = 0;
+
+
+    var lineSymbol = {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 8,
+      strokeColor: '#393'
+    };
+
+
+    //Iniciamos si ya hay valores guardados
+    if ($stateParams.id) {
+      $scope.ruta = userInfo.getRuta($stateParams.id).ruta;
+      $scope.seleccionarPunto = 'Origen';
+      $scope.agregarMarcador($scope.ruta.puntos['Origen']);
+      $scope.seleccionarPunto = 'Destino';
+      $scope.agregarMarcador($scope.ruta.puntos['Destino']);
+    }
+
     var options = {
       timeout: 10000,
       enableHighAccuracy: true
     };
-    /* 
-      $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-     
-        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-     
-        var mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-     
-        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-     
-      }, function(error){
-        console.log("Could not get location");
-      });
-    */
+
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
 
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -51,6 +76,7 @@ angular.module('app.controllers', [])
     var infoWindow = new google.maps.InfoWindow({
       map: map
     });
+    directionsDisplay.setMap(map);
 
 
 
@@ -61,26 +87,34 @@ angular.module('app.controllers', [])
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-
+        $scope.pos = pos;
 
         var flightPath = new google.maps.Polyline({
           path: ruta.puntos,
           geodesic: true,
           strokeColor: '#FF0000',
           strokeOpacity: 1.0,
-          strokeWeight: 2
+          strokeWeight: 5,
+          icons: [{
+            icon: lineSymbol,
+            offset: '100%'
+          }],
         });
+
+
 
         flightPath.setMap(map);
 
+        animateCircle(flightPath);
+
+        flightPath.addListener('click', function (e) {
+          cambiarMarcadores(e.latLng);
+        });
 
         infoWindow.setPosition(pos);
         infoWindow.setContent('Posición Actual.');
         map.setCenter(pos);
-        map.addListener('click', function (e) {
-          cambiarMarcadores(e.latLng);
-          alert($scope.myTitle);
-        });
+
 
       }, function () {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -105,12 +139,20 @@ angular.module('app.controllers', [])
       var marker = new google.maps.Marker({
         position: pos,
         map: map,
-        draggable: true,
+        draggable: false,
         animation: google.maps.Animation.DROP,
         label: $scope.seleccionarPunto
       });
-      if ($scope.gMarkers[$scope.seleccionarPunto]) $scope.gMarkers[$scope.seleccionarPunto].setMap(null);
+
+      if ($scope.gMarkers[$scope.seleccionarPunto]) {
+        $scope.gMarkers[$scope.seleccionarPunto].setMap(null);
+      }
       $scope.gMarkers[$scope.seleccionarPunto] = marker;
+      $scope.ruta.puntos[$scope.seleccionarPunto] = marker.position;
+
+      if ($scope.seleccionarPunto === 'Origen') {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      }
     }
 
 
@@ -126,6 +168,7 @@ angular.module('app.controllers', [])
 
 
 
+
     $scope.seleccionar = function (tipo) {
       if ($scope.seleccionarPunto.length === 0) {
         $scope.seleccionarPunto = tipo;
@@ -136,7 +179,49 @@ angular.module('app.controllers', [])
       }
     }
 
+    $scope.guardarRuta = function (ruta) {
+      userInfo.addRuta(ruta);
+      var alertPopup = $ionicPopup.alert({
+        title: "Terminado",
+        template: "Se a guardado con exito",
+        okText: "OK",
+        okType: "button-balanced"
+      });
+      $timeout(function(){
+        var alertPopup = $ionicPopup.alert({
+        title: "Camión Llegando",
+        template: "Tu Camión llega en 5 minutos",
+        okText: "OK",
+        okType: "button-balanced"
+      });
+      },10000)
+      $state.go('app.rutas');
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      directionsService.route({
+        origin: $scope.pos,
+        destination: $scope.ruta.puntos['Origen'],
+        travelMode: google.maps.TravelMode.WALKING
+      }, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
 
 
+    function animateCircle(line) {
+      var count = 1000;
+      window.setInterval(function () {
+        count = (count + 1) % 2000;
+
+        var icons = line.get('icons');
+        icons[0].offset = (count / 40) + '%';
+        line.set('icons', icons);
+      }, 300);
+    }
 
   });
--- www/js/loginController.js
+++ www/js/loginController.js
@@ -1,7 +1,9 @@
 angular.module('login.controllers', ['ionic','ui.router'])
 
-.controller('LoginCtrl', function ($scope, $ionicModal, $timeout,$state) {
+.controller('LoginCtrl', function ($scope, $ionicModal, $timeout,$state,userInfo) {
     $scope.doLogin = function (user) {
+        userInfo.updateUser(user);
+        alert(user.usuario);
         $state.go('app.rutas');
     };
 })
@@ -19,18 +21,26 @@ angular.module('login.controllers', ['ionic','ui.router'])
             return this.rutas;
         },
         getRuta: function(id){
-            for(var i = 0; i<rutas.length; i++){
-                if(i == id) return rutas[id];
+            for(var i = 0; i<this.rutas.length; i++){
+                if(this.rutas[i].id == id ) return this.rutas[id];
             }
             return null;
         },
         updateRuta: function(id,ruta){
-            for(var i = 0; i<rutas.length; i++){
-                 if( rutas[i] == id) rutas[i] = ruta;
+            for(var i = 0; i<this.rutas.length; i++){
+                 if(this.rutas[i].id == id) this.rutas[i].ruta = ruta;
             }
         },
-        addRuta: function(ruta){
-            var id = rutas.length;
+        addRuta: function(rutaInfo){
+            var no = this.rutas.length;
+            var ruta = {
+                id: no,
+                ruta: rutaInfo
+            }
+            this.rutas.push(ruta);
+            console.log(this.rutas[0]);
+            console.log("Registrado");
+            return no;
         }
     }
 })
